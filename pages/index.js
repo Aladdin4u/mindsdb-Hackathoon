a67e32e746e01c603449e6a254b1fa41d1eb0ff3
aladdin4u <taofikakinkumi@gmail.com>
2023-04-17 15:42:31 +0100

    fetch data from airtable

--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,77 @@
 import MindsDB from "mindsdb-js-sdk";
 
-export default function Home({ response }) {
-  console.log(response);
+export default function Home() {
+  // console.log(response);
   return (
     <div className="text-3xl font-bold underline">Welcome to Next.js!</div>
   );
 }
-async function conectDB() {
+
+export async function getStaticProps(context) {
   const connectionParams = {
-    user: process.env.POSTGRES_USER,
-    port: process.env.POSTGRES_PORT,
-    password: process.env.POSTGRES_PASS,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
+    base_id: process.env.AIRTABLE_BASE_ID,
+    table_name: process.env.AIRTABLE_TABLE_NAME,
+    api_key: process.env.AIRTABLE_API_KEY,
+  };
+  const regressionTrainingOptions = {
+    select: "SELECT * FROM weather",
+    integration: "airtable_datasource",
   };
   try {
-    await MindsDB.connect({
+    const db = await MindsDB.connect({
       user: process.env.MINDDB_USER,
       password: process.env.MINDDB_PASS,
     });
-    const pgDatabase = await MindsDB.Databases.createDatabase(
-      "psql_datasource",
-      "postgres",
-      connectionParams
+    console.log("connected to mindsdb", db);
+    // const airDatabase = await MindsDB.Databases.createDatabase(
+    //   "airtable_datasource",
+    //   "airtable",
+    //   connectionParams
+    // );
+    // console.log("connected to airtable", airDatabase);
+    // let WeatherPredictorModel = await MindsDB.Models.trainModel(
+    //   "weather_predictor_model",
+    //   "weather",
+    //   "mindsdb",
+    //   regressionTrainingOptions
+    // );
+
+    // // // Wait for the training to be complete. This is just a simple example. There are much better ways to do this.
+    // console.log("after awaiting integration", WeatherPredictorModel)
+    // while (WeatherPredictorModel.status === "completed") {
+    // }
+    let WeatherPredictorModel = await MindsDB.Models.getModel(
+      "weather_predictor_model",
+      "mindsdb"
     );
+
+    const queryOptions = {
+      where: [
+        'date = "2012-01-03"',
+        "temp_max = 11.7",
+        "temp_min = 7.2",
+        "wind = 2.3",
+      ],
+    };
+    const weatherPrediction = await WeatherPredictorModel.query(queryOptions);
+    const response = await weatherPrediction;
+    console.log("value ==>",weatherPrediction.value);
+    console.log("wxplain ==>",weatherPrediction.explain);
+    console.log("data ==>",weatherPrediction.data);
+    console.log("res ==>",response);
   } catch (error) {
     console.log(error);
     // Failed to authenticate.
   }
-}
-conectDB();
-export async function getStaticProps(context) {
-  const regressionTrainingOptions = {
-    select: "SELECT * FROM demo_data.home_rentals",
-    integration: "example_db",
-  };
 
-  try {
-    // MindsDB.Models.retrainModel has the same interface for retraining models.
-    // The returned promise resolves when the model is created, NOT when training is actually complete.
-    let homeRentalPriceModel = await MindsDB.Models.trainModel(
-      'home_rentals_model',
-      'rental_price',
-      'mindsdb',
-      regressionTrainingOptions);
+  // try {
+  //   // MindsDB.Models.retrainModel has the same interface for retraining models.
+  //   // The returned promise resolves when the model is created, NOT when training is actually complete.
 
-    // // Wait for the training to be complete. This is just a simple example. There are much better ways to do this.
-    while (homeRentalPriceModel.status === 'training') {
-      homeRentalPriceModel = await MindsDB.Models.getModel(
-        "home_rentals_model",
-        "mindsdb"
-      );
-    }
-
-    const queryOptions = {
-      where: [
-        "sqft = 823",
-        'location = "good"',
-        'neighborhood = "downtown"',
-        "days_on_market = 10",
-      ],
-    };
-
-    const rentalPricePrediction = homeRentalPriceModel.query(queryOptions);
-    const response = rentalPricePrediction.json();
-    console.log(rentalPricePrediction.value);
-    console.log(rentalPricePrediction.explain);
-    console.log(rentalPricePrediction.data);
-  } catch (error) {
-    // Something went wrong training or querying.
-  }
+  // } catch (error) {
+  //   // Something went wrong training or querying.
+  // }
   return {
-    props: { response },
+    props: {},
   };
 }
